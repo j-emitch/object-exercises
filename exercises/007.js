99fa2bdbb15b98f80649c26c99df9877cc32d13b
Joseph Mitchell <joseph.e.mitchell17@gmail.com>
2025-01-23 19:22:07 -0500

    feat: function 7

--- exercises/007.js
+++ exercises/007.js
@@ -49,8 +49,24 @@ var customerData = {
 
 function greetCustomer(firstName) {
   var greeting = '';
-  // your code here
+
+
+  if (!customerData.hasOwnProperty(firstName)) {
+    greeting = 'Welcome! Is this your first time?';
+  }
+  else {
+    var numVisits = customerData[firstName].visits;
+
+    if (numVisits === 1) {
+      greeting = 'Welcome back, ' + firstName + '! We\'re glad you liked us the first time!';
+  }
+    if (numVisits > 1) {
+      greeting = 'Welcome back, ' + firstName + '! So glad to see you again!';
+    }
+  }
+
 
   return greeting;
 }
 
+greetCustomer('James');
